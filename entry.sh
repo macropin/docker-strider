0fe949510e1a83b3e72532258c1a2aea64326e21
Andrew Cutler <macropin@gmail.com>
2015-07-21 19:07:20 +1000

    Make output nicer

--- entry.sh
+++ entry.sh
@@ -12,26 +12,25 @@ set -e
 # MONGO_PORT_27017_TCP_PORT=27017
 # MONGO_PORT_27017_TCP_PROTO=tcp
 
-# Create a DB_URI from linked container variables above
+# Create a DB_URI from linked container. See variables above.
 if [ -z "$DB_URI" ]; then
     export DB_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR-localhost}:${MONGO_PORT_27017_TCP_PORT-27017}/strider"
     echo 'export DB_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR-localhost}:${MONGO_PORT_27017_TCP_PORT-27017}/strider"' > $HOME/.bashrc
-    echo "Set DB_URI=$DB_URI"
+    echo "$(basename $0) >> Set DB_URI=$DB_URI"
 fi
 
 # Update npm cache if no modules exist
-# TODO: make this an incremental update so that upgrades are handled
 if [ ! -d "/data/node_modules" ]; then
-    echo 'Copying node_modules...'
+    echo "$(basename $0) >> Copying node_modules from cache..."
     mkdir -p /data/node_modules
     cp -r --preserve=mode,timestamps,links,xattr /opt/strider/node_modules.cache/* /data/node_modules/
 fi
 
 # Create admin user if variables defined
 if [ ! -z "$STRIDER_ADMIN_EMAIL" -a ! -z "$STRIDER_ADMIN_PASSWORD" ]; then
-    echo "Running addUser"
+    echo "$(basename $0) >> Running addUser"
     strider addUser --email $STRIDER_ADMIN_EMAIL --password $STRIDER_ADMIN_PASSWORD --admin true
-    echo "Created Admin User: $STRIDER_ADMIN_EMAIL, Password: $STRIDER_ADMIN_PASSWORD"
+    echo "$(basename $0) >> Created Admin User: $STRIDER_ADMIN_EMAIL, Password: $STRIDER_ADMIN_PASSWORD"
 fi
 
 echo "Exec'ing command $@"
