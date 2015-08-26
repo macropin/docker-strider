7a4682023ea3c5f0f0a3a6eab0c9056e1dcde5c3
Andrew Cutler <macropin@gmail.com>
2015-08-26 16:32:22 +1000

    cleanup and improve variable default escape

--- entry.sh
+++ entry.sh
@@ -2,20 +2,10 @@
 
 set -e
 
-# These are the variables from a linked MongoDB container
-# MONGO_ENV_MONGO_MAJOR=3.0
-# MONGO_ENV_MONGO_VERSION=3.0.3
-# MONGO_NAME=/strider-app/mongo
-# MONGO_PORT=tcp://172.17.0.15:27017
-# MONGO_PORT_27017_TCP=tcp://172.17.0.15:27017
-# MONGO_PORT_27017_TCP_ADDR=172.17.0.15
-# MONGO_PORT_27017_TCP_PORT=27017
-# MONGO_PORT_27017_TCP_PROTO=tcp
-
 # Create a DB_URI from linked container. See variables above.
 if [ -z "$DB_URI" ]; then
-    export DB_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR-localhost}:${MONGO_PORT_27017_TCP_PORT-27017}/strider"
-    echo 'export DB_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR-localhost}:${MONGO_PORT_27017_TCP_PORT-27017}/strider"' > $HOME/.bashrc
+    export DB_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR:-localhost}:${MONGO_PORT_27017_TCP_PORT:-27017}/strider"
+    echo 'export DB_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR:-localhost}:${MONGO_PORT_27017_TCP_PORT:-27017}/strider"' > $HOME/.bashrc
     echo "$(basename $0) >> Set DB_URI=$DB_URI"
 fi
 
